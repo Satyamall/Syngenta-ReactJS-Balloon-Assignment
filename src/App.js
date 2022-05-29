5d56f2fa92126a23624f467da2798f6a31354af6
Satyamall <thesatyamall123@gmail.com>
2022-05-29 12:59:24 +0530

    Syngenta React JS Balloon assignment code in react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useState } from "react";
 import randomcolor from 'randomcolor';
 
+// Total number circles inside an array with id, randomcolor(),and show options.
 const totalCircles = [
   {
     id: 1,
@@ -33,15 +34,19 @@ const totalCircles = [
 
 function App() {
 
+  // it contains the total circles
   const [circles, setCircles] = useState(totalCircles)
 
+  // it contain the input text in numeric value
   const [text, setText] = useState(null);
 
-  const handleShow = () => {
-    const addCircleToEmptyBox = circles.map((circle) => circle.id === Number(text) ? { ...circle, show: false } : circle)
-    setCircles(addCircleToEmptyBox);
+  // This function show circles inside the empty box
+  const handleShowInEmptyBox = () => {
+    const showCircleToEmptyBox = circles.map((circle) => circle.id === Number(text) ? { ...circle, show: false } : circle)
+    setCircles(showCircleToEmptyBox);
   }
 
+  // This function hide the circle from empty box and show the circle on its original position.
   const handleRemoveCricle = (id) => {
     const removeCircleFromEmptyBox = circles.map((circle) => circle.id === Number(id) ? { ...circle, show: true } : circle)
     setCircles(removeCircleFromEmptyBox);
@@ -50,7 +55,11 @@ function App() {
   return (
     <div className="App">
       <h1>Syngenta React JS Balloon Assignment</h1>
+
+      {/* This is Main Box which contains hole code for all operations of circles */}
       <div className='main-box'>
+
+        {/* This is Empty box code to show and remove circle inside it  */}
         <div className='box'>
           <h3>Empty Box</h3>
           <div className='empty-box'>
@@ -64,6 +73,8 @@ function App() {
             }
           </div>
         </div>
+
+        {/* This is 5 circles box which contains the circles */}
         <div className='box'>
           <h3>{circles.length} Circles</h3>
           {circles?.filter((v) => v.show === true).map((circle) => {
@@ -71,13 +82,15 @@ function App() {
           })
           }
         </div>
+
+        {/* This Box contains the input text which take number as input and shoot button */}
         <div className='box'>
           <h3>A Text Box</h3>
           <input type="number" placeholder="Enter number between 1 to 5" min="1" max={circles.length} className='input-box'
             onChange={(e) => setText(e.target.value)}
           />
           <div>
-            <button className='btn' onClick={handleShow}>Shoot</button>
+            <button className='btn' onClick={handleShowInEmptyBox}>Shoot</button>
           </div>
         </div>
       </div>
